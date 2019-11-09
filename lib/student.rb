10df216f63e609047d77636148dc03425ce4bce9
Baktiar Miah <baktiar.m@gmail.com>
2019-11-09 22:10:32 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -46,6 +46,15 @@ class Student
       student.save
       student
     end
+    
+    def self.new_from_db(row)
+      # create a new Student object given a row from the database
+      new_student = self.new
+      new_student.id=row[0]
+      new_student.name=row[1]
+      new_student.grade=row[2]
+      new_student
+    end
 
     def self.find_by_name(name)
       # find the student in the database given a name
