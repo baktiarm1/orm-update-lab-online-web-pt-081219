e45d8a3c30514d7936db425b353225c970b38bf5
Baktiar Miah <baktiar.m@gmail.com>
2019-11-09 22:26:00 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -54,8 +54,9 @@ def self.create(name:, grade:)
  end
 
  def self.find_by_name(name)
-   sql = "SELECT * FROM students WHERE name = ?"
-   DB[:conn].execute(sql, name).map { |row| new_from_db(row) }.first
+    sql = "SELECT * FROM students WHERE name = ?"
+    DB[:conn].execute(sql, name).map { |row| new_from_db(row) }.first
+  end
  end
 
  def update
